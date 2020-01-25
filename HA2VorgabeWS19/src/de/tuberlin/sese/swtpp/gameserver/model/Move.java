5b57a8e6d063b7452ce1da12998af069151fabb9
Flibusta <60180483+Flibusta@users.noreply.github.com>
2020-01-25 16:05:39 +0100

    Update Move.java. whatFigureToMove() and other stuff

--- HA2VorgabeWS19/src/de/tuberlin/sese/swtpp/gameserver/model/Move.java
+++ HA2VorgabeWS19/src/de/tuberlin/sese/swtpp/gameserver/model/Move.java
@@ -1,6 +1,8 @@
 package de.tuberlin.sese.swtpp.gameserver.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * 
@@ -32,7 +34,75 @@ public class Move implements Serializable {
 		this.board = boardBefore;
 		this.player = player;
 	}
+	
 
+	
+    /*
+     * Returns the code of the figure that we are willing to move with the current Move
+     */
+    public String whatFigureToMove() {
+        String moveStart = this.move.substring(0, 2);
+        ArrayList<String> positionsOfFiguresOnBoard = positionsOfFiguresOnBoard();
+        int index = getIndexOfAPosition(moveStart);
+        String codeOfTheFigure = positionsOfFiguresOnBoard.get(index);
+
+        return codeOfTheFigure;
+    }
+
+    
+    /*
+     * Returns an arraylist of all figures on the current board sorted by their position
+     */
+    public ArrayList<String> positionsOfFiguresOnBoard() {
+        //array of all figures on the current board sorted by their position
+        String[] array;
+        //arraylist with the same contents as the array
+        ArrayList<String> arrayList;
+        String boardState = this.board;
+
+
+        //to get every figure in the board string separated by a comma
+        //we need to remove the '/' sign that separates every row and replace it with a comma
+        char[] boardStateCharArray = boardState.toCharArray();
+        for (int i = 0; i < boardState.length(); i++) {
+            if (boardStateCharArray[i] == '/') {
+                boardStateCharArray[i] = ',';
+            }
+        }
+        boardState = String.valueOf(boardStateCharArray);
+
+        //to get the right position on the board correctly using split function
+        //we need to check if ',' is the last symbol in our string
+        if (boardState.charAt(boardState.length() - 1) == ',') {
+            array = boardState.split(",");
+            arrayList = new ArrayList<String>(Arrays.asList(array));
+            arrayList.add(" ");
+
+        } else {
+            array = boardState.split(",");
+            arrayList = new ArrayList<String>(Arrays.asList(array));
+
+        }
+
+        return arrayList;
+    }
+
+    
+    
+    /*
+     * Returns the index of a certain position in the arraylist created by the
+       positionsOfFiguresOnBoard method
+     */
+    
+    int getIndexOfAPosition(String position) {
+        char column = position.charAt(0);
+        int row = Integer.parseInt(position.substring(1));
+        int index = ((9 - row) * 9) + (column - 'a');
+
+        return index;
+    }
+
+    
 	/************************************
 	 * getters/setters
 	 ************************************/
